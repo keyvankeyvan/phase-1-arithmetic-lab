f7e604d2e453b3b593dcc187aa7624f95bd56a39
Keyvan Alonso <keyvanalonso@gmail.com>
2022-03-15 14:30:18 -0400

    commit tree b0c2a9c177b3b1443eb583cc7396ef4ae92c827e

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+const num3 = 44;
+const num4 = 10;
+
+let mod = num3 % num4;
+
+let max = Math.max(20,1,2,3,4,5,6,7);
